55af0c8eae4af29976fd6dece06e3f91e4594b6f
liha1104 <victorlindback2@yahoo.no>
2015-11-23 14:32:48 +0100

    Test2

--- Test/UtilityTest.java
+++ Test/UtilityTest.java
@@ -5,47 +5,47 @@ import org.junit.Test;
 
 public class UtilityTest {
 
-	@Test
-	public void nullBitLengthTest() {
-		assertThat(Utility.bitToInt(""), is(0));
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void tooLongBitLengthTest() {
-		Utility.bitToInt("1111111111111111111111111111111111111111111111111111111111111111");
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void bitStringContainsSomethingOtherThan1or0Test() {
-		Utility.bitToInt("23456");
-	}
-	
-	@Test
-	public void intToBit_Returns_Correctly() {
-		assertThat(Utility.bitToInt("00001010"), is(10));
-	}
-
-
-
-
     @Test
-    public void nullHexLengthTest() {
+    public void nullBitLengthTest() {
         assertThat(Utility.bitToInt(""), is(0));
     }
 
     @Test(expected = IllegalArgumentException.class)
+    public void tooLongBitLengthTest() {
+        Utility.bitToInt("1111111111111111111111111111111111111111111111111111111111111111");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void bitStringContainsSomethingOtherThan1or0Test() {
+        Utility.bitToInt("23456");
+    }
+
+    @Test
+    public void BitToInt_Returns_Correctly() {
+        assertThat(Utility.bitToInt("00001010"), is(10));
+    }
+
+    @Test
+    public void nullHexLengthTest() {
+        assertThat(Utility.hexToInt(""), is(0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
     public void tooLongHexLengthTest() {
-        Utility.bitToInt("123456789");
+        Utility.hexToInt("123456789");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void HexStringContainsSomethingWrongTest() {
-        Utility.bitToInt("something wrong");
+        Utility.hexToInt("HG14A");
     }
 
     @Test
-    public void intToHex_Returns_Correctly() {
-        assertThat(Utility.bitToInt("111D7A"), is(1121658));
+    public void HexToInt_Returns_Correctly() {
+        assertThat(Utility.hexToInt("a"), is(10));
     }
+
+    @Test
+    public void BitToInt_Returns_Corectly() {assertThat(Utility.intToHex(2721), is("AA1"));}
 
 }
--- src/Utility.java
+++ src/Utility.java
@@ -1,31 +1,55 @@
 
 public class Utility {
 
-	public static int bitToInt(String bit) {
-		char[] numbers = bit.toCharArray();
-		if(24 < bit.length()) 
-			throw new IllegalArgumentException("Bit is too long");
-		if (den inneholder noe som ikke er en eller null)
-			throw new IllegalArgumentException("Bit contains something else than 0 and 1");
-		 
-		    Integer result = 0;
-		    int count = 0;
-		    for(int i=numbers.length-1;i>=0;i--){
-		         if(numbers[i]=='1')result+=(int)Math.pow(2, count);
-		         count++;
-		    }
-		    return result;
-	}
+    public static int bitToInt(String bit) {
+        char[] numbers = bit.toCharArray();
+        if (24 < bit.length())
+            throw new IllegalArgumentException("Bit is too long");
+        if (bit == "")
+            return 0;
+        if (!bit.matches("^[0-1_]+$"))
+            throw new IllegalArgumentException("Bit contains something else than 0 or 1");
+
+        Integer result = 0;
+        int count = 0;
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            if (numbers[i] == '1') result += (int) Math.pow(2, count);
+            count++;
+        }
+        return result;
+    }
 
     public static int hexToInt(String hex) {
-        char[] numbers = bit.toCharArray();
+        char[] numbers = hex.toCharArray();
         if (6 < hex.length())
-            throw new IllegalArgumentException("Bit is too long");
-        if (den inneholder noe som ikke er i en hex streng)
-        throw new IllegalArgumentException("Bit contains something wrong");
+            throw new IllegalArgumentException("Hex is too long");
+        if (hex == "")
+            return 0;
+        if (!hex.matches("^[a-fA-F0-9_]+$"))
+            throw new IllegalArgumentException("Hex contains something wrong");
 
-
-
+        String digits = "0123456789ABCDEF";
+        hex = hex.toUpperCase();
+        int val = 0;
+        for (int i = 0; i < hex.length(); i++) {
+            char c = hex.charAt(i);
+            int d = digits.indexOf(c);
+            val = 16*val + d;
+        }
+        return val;
     }
-	
+
+    public static String intToHex(int num){
+        String digits = "0123456789ABCDEF";
+        if (num == 0) return "0";
+        String hex = "";
+        while (num > 0) {
+            int digit = num % 16;                // rightmost digit
+            hex = digits.charAt(digit) + hex;  // string concatenation
+            num = num / 16;
+        }
+        return hex;
+    }
+
+
 }
